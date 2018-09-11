98224dd4dd636e5b18b7d20708ab979059ceeae9
Sang-Chun Ahn <sangchunahn@yahoo.com>
2018-09-11 14:15:24 -0600

    added angular to display data

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             // add the database context to the startup file to initialize it
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace DatingApp.API
                 // app.UseHsts();
             }
 
+            // allows for cross orgin
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             // app.UseHttpsRedirection();
             // middleware software that connects network base requests
             app.UseMvc();
